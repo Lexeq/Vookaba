a5edbb0c9084c3477b22a721273a081ec3d494a3
Alex <alexandr.mail@abc.com>
2020-10-31 20:43:33 +0300

    Add nlog

--- OakChan/Program.cs
+++ OakChan/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace OakChan
 {
@@ -21,6 +22,6 @@ namespace OakChan
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseNLog();
     }
 }
