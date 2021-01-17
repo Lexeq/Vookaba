2c0b288902983c32f62729004fa04fa707341e46
Alex <alexandr.mail@abc.com>
2021-01-17 23:13:31 +0300

    Show local time to client

---
+++ OakChan/Utils/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OakChan.Utils
+{
+    public static class DateTimeExtensions
+    {
+        public static long GetUnixEpochOffset(this DateTime dateTime)
+        {
+            return (long)(dateTime - DateTime.UnixEpoch).TotalMilliseconds;
+        }
+    }
+}
---
+++ OakChan/wwwroot/scripts/dates.js
@@ -0,0 +1,16 @@
+﻿ToClientTime();
+
+function ToClientTime() {
+    let list = document.getElementsByClassName('post__date');
+    for (let item of list) {
+        item.textContent = FormatTimeOffset(Number.parseInt(item.dataset.timestamp));
+    }
+
+    function FormatTimeOffset(milliseconds) {
+        let padNum = (str) => { return str.toString().length < 2 ? "0" + str : str; };
+        let days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+        let d = new Date(milliseconds);
+        return `${days[d.getDay()]} ${padNum(d.getDate())}/${padNum(d.getMonth() + 1)}/${padNum(d.getFullYear() % 100)}`
+            + ` ${padNum(d.getHours())}:${padNum(d.getMinutes())}:${padNum(d.getSeconds())}`;
+    }
+}
