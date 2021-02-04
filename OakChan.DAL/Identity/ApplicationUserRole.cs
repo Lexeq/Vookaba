81bcfb2b1ee6c8c4de1a1927e88664667375442d
Alex <alexandr.mail@abc.com>
2021-02-04 20:01:39 +0300

    Add identity classes

---
+++ OakChan.DAL/Identity/ApplicationUserRole.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OakChan.Identity
+{
+    public class ApplicationUserRole : IdentityUserRole<int>
+    { }
+}
---
+++ OakChan.DAL/Identity/ApplicationUserStore.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using OakChan.DAL.Database;
+
+namespace OakChan.Identity
+{
+    public class ApplicationUserStore : UserStore<ApplicationUser, ApplicationRole, OakDbContext, int, ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin, ApplicationUserToken, ApplicationRoleClaim>
+    {
+        public ApplicationUserStore(OakDbContext context, IdentityErrorDescriber describer = null)
+            : base(context, describer)
+        { }
+
+        public override int ConvertIdFromString(string id)
+        {
+            if (id == null)
+            {
+                return default;
+            }
+            return int.Parse(id);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
