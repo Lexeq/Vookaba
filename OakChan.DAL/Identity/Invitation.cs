ad1996d54a011ab6f80a78403dd6fcd5040fa215
Alex <alexandr.mail@abc.com>
2021-09-05 20:40:41 +0300

    Add interfaces

---
+++ OakChan.DAL/Identity/Invitation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace OakChan.Identity
+{
+    public class Invitation<TKey> where TKey : struct, IEquatable<TKey> 
+    {
+        public Guid Id { get; set; }
+
+        public TKey PublisherId { get; set; }
+
+        public IdentityUser<TKey> Publisher { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Expire { get; set; }
+
+        public TKey? UsedByID { get; set; }
+
+        public IdentityUser<TKey> UsedBy { get; set; }
+
+        public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+
+    }
+
+}
