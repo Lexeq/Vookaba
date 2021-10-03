0ca807114d0fe6a925a601d4d7701ab58f7a7ec2
Alexandr <27781977+Lexeq@users.noreply.github.com>
2021-10-03 23:15:20 +0300

    Basic functions for localization in js

---
+++ OakChan/wwwroot/scripts/lang.js
@@ -0,0 +1,14 @@
+﻿//TODO: use local storage?
+//TODO: detect locale
+//TODO: store lang pack on server in static json?
+window.settings = {};
+window.settings.locale = 'ru';
+
+const localizedStrings = {
+    en: { show: "Show", hide: "Hide" },
+    ru: { show: "Показать", hide: "Скрыть" }
+}
+
+function getLocalizedString(str) {
+    return localizedStrings[window.settings.locale][str] || str;
+}
