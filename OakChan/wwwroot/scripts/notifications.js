763bd3173e4f8d184ebae9bad00b45dc9cbd4eb8
Alexandr <27781977+Lexeq@users.noreply.github.com>
2021-11-30 17:11:59 +0300

    Add notifier

---
+++ OakChan/wwwroot/scripts/notifications.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(() => {
+    let notifier = $("<div>");
+    notifier.prop("id", "notifier")
+    notifier.addClass('notifier');
+    $('body').append(notifier);
+});
+
+function showNotification(text) {
+    let note = $("<div>")
+        .addClass("notification")
+        .append(text);
+
+    $('#notifier').append(note);
+    setTimeout(() => {
+        note.remove();
+    }, 5000);
+}
