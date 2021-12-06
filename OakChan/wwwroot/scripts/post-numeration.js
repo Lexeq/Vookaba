1671070573563db92aae5b96e66bb7c2d7474292
Alexandr <27781977+Lexeq@users.noreply.github.com>
2021-12-06 17:01:21 +0300

    Add post numeration in thread

---
+++ OakChan/wwwroot/scripts/post-numeration.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(_ => {
+    if ($('.threads-container').length > 0) {
+        return;
+    }
+    let details = $('body > div > .thread .post__details',);
+    for (let i = 0; i < details.length;) {
+        $(details[i]).prepend($(`<span>${++i}</span>`).addClass('numbering'));
+    }
+});
