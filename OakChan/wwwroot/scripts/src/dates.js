2ef3ad8a7f69415229b00113642f6a1bdd3cd1d1
Alexandr <27781977+Lexeq@users.noreply.github.com>
2022-01-10 23:03:45 +0300

    Fix typo

--- OakChan/wwwroot/scripts/src/dates.js
+++ OakChan/wwwroot/scripts/src/dates.js
@@ -9,7 +9,7 @@ function ToClientTime() {
     //Todo date format
     function FormatTimeOffset(milliseconds) {
         let padNum = (str) => { return str.toString().length < 2 ? "0" + str : str; };
-        let days = localizedStrings.daysShot;
+        let days = localizedStrings.daysShort;
         let d = new Date(milliseconds);
         return `${days[d.getDay()]} ${padNum(d.getDate())}/${padNum(d.getMonth() + 1)}/${padNum(d.getFullYear() % 100)}`
             + ` ${padNum(d.getHours())}:${padNum(d.getMinutes())}:${padNum(d.getSeconds())}`;
