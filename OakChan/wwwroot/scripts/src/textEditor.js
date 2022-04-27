c5e43a0cf71780f06dd2865290c70cdfb85c5a7d
Alexandr <27781977+Lexeq@users.noreply.github.com>
2022-04-27 21:09:34 +0300

    Add markup buttons

---
+++ OakChan/wwwroot/scripts/src/textEditor.js
@@ -0,0 +1,15 @@
+﻿function textEditorAddTag(tag1, tag2) {
+    let textArea = $('#message-text')[0];
+    let text = textArea.value;
+    let start = textArea.selectionStart;
+    let end = textArea.selectionEnd;
+    text = text.slice(0, start) +
+        tag1 +
+        text.slice(start, end) +
+        tag2 +
+        text.slice(end);
+    textArea.value = text;
+    textArea.focus();
+    textArea.selectionStart = start + tag1.length;
+    textArea.selectionEnd = end + tag1.length;
+}
