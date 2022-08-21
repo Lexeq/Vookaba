e4b4160e1d930719a5ea6ec8f3ba380167220eb1
Alexandr <27781977+Lexeq@users.noreply.github.com>
2022-08-21 19:01:30 +0300

    Time parser

--- Vookaba.Common/Extensions/DateTimeExtensions.cs
+++ Vookaba.Common/Extensions/DateTimeExtensions.cs
@@ -1,9 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Vookaba.Common.Extensions
 {
     public static class DateTimeExtensions
     {
+        private enum State { Begin, Number, Literal }
+
+        private readonly static IReadOnlyDictionary<string, Func<DateTime, int, DateTime>> acts = new Dictionary<string, Func<DateTime, int, DateTime>>()
+        {
+            ["y"] = (dt, val) => dt.AddYears(val),
+            ["d"] = (dt, val) => dt.AddDays(val),
+            ["h"] = (dt, val) => dt.AddHours(val),
+            ["m"] = (dt, val) => dt.AddMinutes(val),
+        };
+
+        public static DateTime AddFromString(this DateTime dateTime, string range)
+        {
+            var state = State.Begin;
+            var num = 0;
+
+            for (int i = 0; i < range.Length;)
+            {
+                switch (state)
+                {
+                    case State.Begin:
+                        if (range[i] == ' ')
+                        {
+                            i++;
+                            continue;
+                        }
+                        if (!char.IsDigit(range[i]))
+                        {
+                            throw new FormatException($"Digit was expected at position {i} but was {range[i]}.");
+                        }
+                        state = State.Number;
+                        break;
+                    case State.Number:
+                        if (char.IsDigit(range[i]))
+                        {
+                            num = num * 10 + (range[i] - '0');
+                            i++;
+                        }
+                        else
+                        {
+                            state = State.Literal;
+                        }
+                        break;
+                    case State.Literal:
+                        //TODO: support literals with lenght > 1
+                        var literal = range[i].ToString();
+                        if (acts.ContainsKey(literal))
+                        {
+                            dateTime = acts[literal].Invoke(dateTime, num);
+                            i++;
+                            num = 0;
+                            state = State.Begin;
+                        }
+                        else
+                        {
+                            throw new FormatException($"Unknown literal '{range[i]}' at position {i}.");
+                        }
+                        break;
+                    default:
+                        throw new ApplicationException("Invalid state.");
+                }
+            }
+            return dateTime;
+        }
+
         public static long GetUnixEpochOffset(this DateTime dateTime)
         {
             return (long)(dateTime - DateTime.UnixEpoch).TotalMilliseconds;
---
+++ Vookaba.Tests/Unit/AddDurationTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using Vookaba.Common.Extensions;
+
+namespace Vookaba.Tests.Unit
+{
+    public class AddDurationTests
+    {
+        [TestCaseSource(nameof(Cases))]
+        public void AddTest(DateTime original, string input, DateTime expected)
+        {
+            var result = original.AddFromString(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        static readonly object[] Cases =
+        {
+            new object[] { new DateTime(2000, 1, 1), "2d", new DateTime(2000, 1, 3) },
+            new object[] { new DateTime(2000, 1, 1), "2d2m", new DateTime(2000, 1, 3,0,2,0) },
+            new object[] { new DateTime(2000, 1, 1), "2d  2m", new DateTime(2000, 1, 3,0,2,0) },
+            new object[] { new DateTime(2000, 1, 1), "2d2d", new DateTime(2000, 1, 5) }
+        };
+    }
+}
